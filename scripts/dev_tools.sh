250f9bd8670dfa722029161a4c43dd0303f9dd9f
Ruy-Araujo-Personal <ruy.araujo@yahoo.com.br>
2023-11-16 04:15:19 -0300

    feat: Add dev tools

---
+++ scripts/dev_tools.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+highlighted_message() {
+  message="$1" 
+  echo "▁▂▃▅▆▓▒░ $message ░▒▓▆▅▃▂▁"
+}
+
+
+# Vscode
+highlighted_message "Installing Vscode..."
+sudo apt install snapd
+sudo snap install code --classic
+highlighted_message "Vscode installed successfully."
+
+# Postman
+highlighted_message "Installing Postman..."
+sudo snap install postman
+highlighted_message "Postman installed successfully."
+
+# Python PIP
+highlighted_message "Installing Python PIP..."
+sudo apt install python3-pip
+highlighted_message "Python PIP installed successfully."
+
+# Python Virtualenv
+highlighted_message "Installing Python Virtualenv..."
+sudo apt install python3-virtualenv
+highlighted_message "Python Virtualenv installed successfully."
+
