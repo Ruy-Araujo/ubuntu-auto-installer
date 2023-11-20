ae23ca2fd7f9bea940a535512230440965c87b3e
Ruy-Araujo-Personal <ruy.araujo@yahoo.com.br>
2023-11-20 17:21:45 -0300

    fix: Dbeaver message

--- scripts/data.sh
+++ scripts/data.sh
@@ -11,4 +11,4 @@ wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | tee /etc/apt/sources.list.d/dbeaver.list
 apt update
 apt install dbeaver-ce -y
-highlighted_message "Docker installed DBeaver."
+highlighted_message "DBeaver installed successfully."
