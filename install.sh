e0b2201a74d96d244d4fe82affb492c88c9e8811
Ruy-Araujo-Personal <ruy.araujo@yahoo.com.br>
2023-11-20 17:01:07 -0300

    feat: Add new scripts

--- install.sh
+++ install.sh
@@ -57,38 +57,54 @@ chmod +x ./scripts/*.sh
 
 # Basic Tools
 highlighted_message "Installing Basic Tools..."
-./scripts/basic_tools.sh
+./scripts/basic.sh
 highlighted_message "Basic Tools installed successfully."
 
-# Terminal Tools
-highlighted_message "Installing Terminal Tools..."
-./scripts/terminal_tools.sh
-highlighted_message "Terminal Tools installed successfully."
-
 # Dev Tools
-highlighted_message "Installing Dev Tools..."
-./scripts/dev_tools.sh
-highlighted_message "Dev Tools installed successfully."
+highlighted_message "Installing Developer Tools..."
+./scripts/developer.sh
+highlighted_message "Developer Tools installed successfully."
 
 # infra-tools
-highlighted_message "Installing Infra Tools..."
-./scripts/infra_tools.sh
-highlighted_message "Infra Tools installed successfully."
+highlighted_message "Installing Infrastructure Tools..."
+./scripts/infrastructure.sh
+highlighted_message "Infra Tools Installed successfully."
 
 # Azure Tools
 highlighted_message "Installing Azure Tools..."
-./scripts/azure_tools.sh
+./scripts/azure.sh
 highlighted_message "Azure Tools installed successfully."
 
 # Data Tools
 highlighted_message "Installing Data Tools..."
-./scripts/data_tools.sh
+./scripts/data.sh
 highlighted_message "Data Tools installed successfully."
 
 # Web Tools
 highlighted_message "Installing Web Tools..."
-./scripts/web_tools.sh
+./scripts/web.sh
 highlighted_message "Web Tools installed successfully."
+
+# Gnome-Extensions
+highlighted_message "Installing Gnome Extensions..."
+./scripts/gnome-extensions.sh
+highlighted_message "Gnome Extensions installed successfully."
+
+# Outhers
+highlighted_message "Installing Outhers Tools..."
+./scripts/outhers.sh
+highlighted_message "Outhers Tools installed successfully."
+
+# Sound & Video
+highlighted_message "Installing Sound & Video Tools..."
+./scripts/sound-video.sh
+highlighted_message "Sound & Video Tools installed successfully."
+
+# Terminal Tools
+highlighted_message "Installing Terminal Tools..."
+./scripts/terminal.sh
+highlighted_message "Terminal Tools installed successfully."
+
 
 # Post-install
 
